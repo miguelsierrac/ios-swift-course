1aa9a2d00228ae3804910237183a8343a7181014
Miguel Sierra <msierra@veritran.com>
2018-04-21 12:19:50 -0500

    Se suben cambios realizados en MyFirstApp

--- MyFirstApp/MyFirstApp/ViewController.swift
+++ MyFirstApp/MyFirstApp/ViewController.swift
@@ -7,19 +7,46 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var button: UIButton!
+    
+    var catSound : AVAudioPlayer = AVAudioPlayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        label.isHidden = true;
+        button.imageView?.contentMode = .scaleAspectFit
+        
+        let soundFile = Bundle.main.path(forResource: "Cat", ofType: "wav")
+        
+        do {
+            try catSound = AVAudioPlayer(contentsOf: URL(fileURLWithPath: soundFile!))
+        }
+        catch {
+            print(error)
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func meow(_ sender: Any) {
+        catSound.play()
+        label.isHidden = false
+        
+        Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(hideLabel), userInfo: nil, repeats: false)
+    }
+    
+    @objc func hideLabel() {
+        label.isHidden = true
+    }
+    
 }
 
